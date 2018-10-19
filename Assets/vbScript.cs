2cda6b4b19aac0175a8633cf12cd5852e166f5ec
Fenikkel <paufenollosa@gmail.com>
2018-10-19 12:20:48 +0200

    Initial commit

---
+++ Assets/vbScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vButtonObject;
+    private GameObject cubo;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        vButtonObject = GameObject.Find("actionButton");
+        cubo = GameObject.Find("cuboide");
+        vButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		
+	}
+	
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        cubo.SetActive(false);
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        cubo.SetActive(true);
+    }
+}
